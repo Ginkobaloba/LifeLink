6d20153dbd1e844435804b35b5fc9b56208a93a3
ArsalonAmini <aminihajibasa@gmail.com>
2016-10-10 17:49:00 -0500

    front end functionality - Home view Index update: added four responsive <diVs> at col-sm-3 twitter feed, questionnaire create, appointment create, addresses index

---
+++ LifeLink/Migrations/201610102011416_initial.cs
@@ -0,0 +1,200 @@
+namespace LifeLink.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Addresses",
+                c => new
+                    {
+                        AddressId = c.Int(nullable: false, identity: true),
+                        FirstName = c.String(nullable: false),
+                        LastName = c.String(nullable: false),
+                        Address1 = c.String(nullable: false),
+                        Address2 = c.String(),
+                        City = c.String(nullable: false),
+                        ZipCode = c.Int(nullable: false),
+                        PhoneNumber = c.String(),
+                        Latitude = c.Double(nullable: false),
+                        Longitude = c.Double(nullable: false),
+                        ClosestLocationId = c.Int(nullable: false),
+                        UserId = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.AddressId)
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.AspNetUsers",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        LanguageCode = c.String(),
+                        Email = c.String(maxLength: 256),
+                        EmailConfirmed = c.Boolean(nullable: false),
+                        PasswordHash = c.String(),
+                        SecurityStamp = c.String(),
+                        PhoneNumber = c.String(),
+                        PhoneNumberConfirmed = c.Boolean(nullable: false),
+                        TwoFactorEnabled = c.Boolean(nullable: false),
+                        LockoutEndDateUtc = c.DateTime(),
+                        LockoutEnabled = c.Boolean(nullable: false),
+                        AccessFailedCount = c.Int(nullable: false),
+                        UserName = c.String(nullable: false, maxLength: 256),
+                    })
+                .PrimaryKey(t => t.Id)
+                .Index(t => t.UserName, unique: true, name: "UserNameIndex");
+            
+            CreateTable(
+                "dbo.AspNetUserClaims",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        ClaimType = c.String(),
+                        ClaimValue = c.String(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.AspNetUserLogins",
+                c => new
+                    {
+                        LoginProvider = c.String(nullable: false, maxLength: 128),
+                        ProviderKey = c.String(nullable: false, maxLength: 128),
+                        UserId = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => new { t.LoginProvider, t.ProviderKey, t.UserId })
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.AspNetUserRoles",
+                c => new
+                    {
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        RoleId = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => new { t.UserId, t.RoleId })
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .ForeignKey("dbo.AspNetRoles", t => t.RoleId, cascadeDelete: true)
+                .Index(t => t.UserId)
+                .Index(t => t.RoleId);
+            
+            CreateTable(
+                "dbo.Appointments",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        title = c.String(),
+                        start = c.DateTime(nullable: false),
+                        end = c.DateTime(nullable: false),
+                        Status = c.String(),
+                        LocationId = c.Int(nullable: false),
+                        UserId = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.id)
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId)
+                .ForeignKey("dbo.Locations", t => t.LocationId, cascadeDelete: true)
+                .Index(t => t.LocationId)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.Locations",
+                c => new
+                    {
+                        LocationId = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        StreetAddress = c.String(),
+                        LocationLong = c.Double(nullable: false),
+                        LocationLat = c.Double(nullable: false),
+                    })
+                .PrimaryKey(t => t.LocationId);
+            
+            CreateTable(
+                "dbo.ClientInfoes",
+                c => new
+                    {
+                        ClientInfoId = c.Int(nullable: false, identity: true),
+                        BloodType = c.String(),
+                        Approved = c.Boolean(nullable: false),
+                        UserId = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.ClientInfoId)
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.Questionnaires",
+                c => new
+                    {
+                        QuestionnaireId = c.Int(nullable: false, identity: true),
+                        GeneralHealth1 = c.Boolean(nullable: false),
+                        DonationHistory2 = c.Boolean(nullable: false),
+                        VaxOrShots3 = c.Boolean(nullable: false),
+                        Pregnant4 = c.Boolean(nullable: false),
+                        Medications5 = c.Boolean(nullable: false),
+                        Weight6 = c.Boolean(nullable: false),
+                        RiskySex7 = c.Boolean(nullable: false),
+                        TatooOrPiercing8 = c.Boolean(nullable: false),
+                        Jail9 = c.Boolean(nullable: false),
+                        Needles10 = c.Boolean(nullable: false),
+                        ClientInfoId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.QuestionnaireId)
+                .ForeignKey("dbo.ClientInfoes", t => t.ClientInfoId, cascadeDelete: true)
+                .Index(t => t.ClientInfoId);
+            
+            CreateTable(
+                "dbo.AspNetRoles",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(nullable: false, maxLength: 256),
+                    })
+                .PrimaryKey(t => t.Id)
+                .Index(t => t.Name, unique: true, name: "RoleNameIndex");
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.AspNetUserRoles", "RoleId", "dbo.AspNetRoles");
+            DropForeignKey("dbo.Questionnaires", "ClientInfoId", "dbo.ClientInfoes");
+            DropForeignKey("dbo.ClientInfoes", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.Appointments", "LocationId", "dbo.Locations");
+            DropForeignKey("dbo.Appointments", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.Addresses", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserRoles", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserLogins", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserClaims", "UserId", "dbo.AspNetUsers");
+            DropIndex("dbo.AspNetRoles", "RoleNameIndex");
+            DropIndex("dbo.Questionnaires", new[] { "ClientInfoId" });
+            DropIndex("dbo.ClientInfoes", new[] { "UserId" });
+            DropIndex("dbo.Appointments", new[] { "UserId" });
+            DropIndex("dbo.Appointments", new[] { "LocationId" });
+            DropIndex("dbo.AspNetUserRoles", new[] { "RoleId" });
+            DropIndex("dbo.AspNetUserRoles", new[] { "UserId" });
+            DropIndex("dbo.AspNetUserLogins", new[] { "UserId" });
+            DropIndex("dbo.AspNetUserClaims", new[] { "UserId" });
+            DropIndex("dbo.AspNetUsers", "UserNameIndex");
+            DropIndex("dbo.Addresses", new[] { "UserId" });
+            DropTable("dbo.AspNetRoles");
+            DropTable("dbo.Questionnaires");
+            DropTable("dbo.ClientInfoes");
+            DropTable("dbo.Locations");
+            DropTable("dbo.Appointments");
+            DropTable("dbo.AspNetUserRoles");
+            DropTable("dbo.AspNetUserLogins");
+            DropTable("dbo.AspNetUserClaims");
+            DropTable("dbo.AspNetUsers");
+            DropTable("dbo.Addresses");
+        }
+    }
+}
