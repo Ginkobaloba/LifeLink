8eaf7db4f0fe0b2ffff9caa48115058f3a55a055
ArsalonAmini <aminihajibasa@gmail.com>
2016-10-10 08:39:54 -0500

    recaptcha.cs file update and register(actionlink) update

--- LifeLink/Controllers/AccountController.cs
+++ LifeLink/Controllers/AccountController.cs
@@ -157,7 +157,13 @@ namespace LifeLink.Controllers
                 if (result.Succeeded)
                 {
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
-                    
+
+                    string EncodedResponse = Request.Form["g-Recaptcha-Response"];
+                    bool IsCaptchaValid = (Recaptcha.Validate(EncodedResponse) == "True" ? true : false);
+                    if (IsCaptchaValid)
+                    {
+                        //valid request
+                    }
                     // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
                     // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
---
+++ LifeLink/Controllers/reCaptcha.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace LifeLink.Controllers
+{
+    public class Recaptcha
+    {
+        public static string Validate(string EncodedResponse)
+        {
+            var client = new System.Net.WebClient();
+            string PrivateKey = "6LfDnQgUAAAAAAYaPIFqzjgzvTOJ3gsNlWW0_LOB";
+            var GoogleReplay = client.DownloadString(string.Format("https://www.google.com/recaptcha/api/siteverify?secret={0}&response={1}", PrivateKey, EncodedResponse));
+            var captchaResponse = Newtonsoft.Json.JsonConvert.DeserializeObject<Recaptcha>(GoogleReplay);
+            return captchaResponse.Success;
+        }
+
+        [JsonProperty("success")]
+        public string Success
+        {
+            get { return m_Success; }
+            set { m_Success = value; }
+        }
+
+        private string m_Success;
+        [JsonProperty("error-codes")]
+        public List<string> ErrorCodes
+        {
+            get { return m_ErrorCodes; }
+            set { m_ErrorCodes = value; }
+        }
+
+        private List<string> m_ErrorCodes;
+    }
+}
